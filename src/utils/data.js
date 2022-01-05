5687609f4ae80db398b6c5581497ae7e445fb0f3
Abdullah M. Al khareji <akhareji@cc.mof.gov.kw>
2022-01-05 13:06:55 +0300

    updated utils/data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -18,3 +18,30 @@ export const menu = [
     link: "Projects",
   },
 ];
+
+export const education = [
+  {
+    id: "phs",
+    name: "Palestine High School",
+    major: "High School Deploma",
+    start: "2008",
+    end: "2011",
+    location: "Kuwait",
+  },
+  {
+    id: "acm",
+    name: "American College of the Middle East (ACM)",
+    major: "Information Technology",
+    start: "2012",
+    end: "2015",
+    location: "Kuwait",
+  },
+  {
+    id: "aum",
+    name: "American Universty of the Middle East (AUM)",
+    major: "Information System and Technology",
+    start: "2015",
+    end: "2018",
+    location: "Kuwait",
+  },
+];
